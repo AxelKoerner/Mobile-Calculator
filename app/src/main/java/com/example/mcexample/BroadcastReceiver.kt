adf7498d9a42a61614b9f80d68160822e2f990a5
AxelKoerner <91koax1bif@hft-stuttgart.de>
2025-04-22 23:13:38 +0200

    added comment with explanation regarding the issue with displaying toast messages

--- app/src/main/java/com/example/mcexample/BroadcastReceiver.kt
+++ app/src/main/java/com/example/mcexample/BroadcastReceiver.kt
@@ -8,6 +8,8 @@ import android.widget.Toast
 class SensorBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val message = intent.getStringExtra("message") ?: return
+        //the Toast is not being displayed when the App is closed or in the background, atleast on my emulated device.
+        // I get this error in "Logcat: Suppressing toast from package com.example.mcexample by user request." Sadly I did not find the solution to displaying these messages
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 }
